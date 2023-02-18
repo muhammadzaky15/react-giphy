8e8a52540eae1aba90d5cb990648c7b38a111004
muhammadzakyhol <muhammad.zaky.pnl@gmail.com>
2023-02-18 19:24:53 +0700

    add skeleton

--- src/component/GifList.jsx
+++ src/component/GifList.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import GifItem from "./GitItem";
+import Skeleton from "./Skeleton";
 
 export default function GifList({ data }) {
   useEffect(() => {
     console.log("data", data);
   }, [data]);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
-        {data && data.slice(0, 9).map((item, key) => {
-            return <GifItem gifUrl={item.images.preview_gif.url} />
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+      {data &&
+        data.slice(0, 9).map((item, key) => {
+          return <GifItem gifUrl={item.images.preview_gif.url} />;
         })}
+      {!data && <Skeleton />}
     </div>
   );
 }
